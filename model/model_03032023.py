0cc916a4ead0e9c363868e6087f40cd88163ff52
Shannon! <shannonlei0321@gmail.com>
2025-03-03 22:54:24 -0600

    added mobilenet python; updated model code

--- model/model_03032023.py
+++ model/model_03032023.py
@@ -1,39 +1,54 @@
 import os
+import torch
 import torchvision
 import torchvision.transforms as transforms
 import pandas as pd
+from mobilenetv3 import MobileNetV3_Small
 from PIL import Image
-
-target_size = (930, 930)
 
 # NOTE: load image first, then transform, instead of transforming all possible images at once?
 
-# Turning all the images into a custom Dataset object
-image_dataset = torchvision.datasets.ImageFolder(root = '/home/bqtx/Documents/VLSI/ir_drop_ml/training_data/png-files')
-print(image_dataset)
 
 # Custom Padding Transformer (FIX THIS)
-class PadToSize():
-    def init(self, target_size):
-        self.target_size = target_size
+class PadToSize:
+    def __init__(self, target):
+        self.target = target
 
-    def call(self, img):
+    def __call__(self, img):
         # Calculate padding sizes
-        width, height = img.size
-        pad_width = max(0, self.target_size[0] - width)
-        pad_height = max(0, self.target_size[1] - height)
+        print(img.size())
+        colors, width, height = img.size()
+        pad_left = (max(0, self.target[0] - width)//2)
+        pad_right = ((max(0, self.target[0]-width)+1)//2)
+        pad_top = (max(0, self.target[1] - height)//2)
+        pad_bot = ((max(0,self.target[1] - height)+1)//2)
 
-        padding = (0, 0, pad_width, pad_height) # TODO: FIX HEIGHT & WIDTH CALC
-        return transforms.functional.pad(img, padding, fill=0, padding_mode='constant') # TODO: Change to reflection
+        padding = (pad_left, pad_top, pad_right, pad_bot)
+        return transforms.functional.pad(img, padding, fill=0, padding_mode='reflect')
     
 # Padding Transform Pipeline
-transform = transforms.Compose([
+target_size = (930, 930)
+pipeline= transforms.Compose([
     transforms.ToTensor(),  # Convert image to tensor
+    transforms.Lambda(lambda x: x[:3]),
     PadToSize(target_size),  # Pad the image to the target size
-    transforms.ToPILImage()  # Convert back to PIL image (optional)
+    # transforms.ToPILImage(), 
 ])
 
+# Turning all the images into a custom Dataset object
+image_dataset = torchvision.datasets.ImageFolder(root = '/home/bqtx/Documents/VLSI/ir_drop_ml/training_data/png-files', transform = pipeline)
+#print(image_dataset)
+
 image = Image.open('/home/bqtx/Documents/VLSI/ir_drop_ml/training_data/png-files/current/current_map00_current.png')
-transformed_image = transform(image)
-transformed_image.show()
+# temp = transforms.ToTensor()
+# print(temp(image))
+transformed_image = pipeline(image).size()
+print(transformed_image)
+#transformed_image.show()
 #torchvision.transforms.Pad(padding, fill=0, padding_mode='constant')
+
+network = MobileNetV3_Small(2)
+# print(network)
+
+# random = torch.rand(2,3,224,224)
+network(transformed_image)
